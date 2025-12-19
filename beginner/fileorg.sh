996c4a5925092b31e5a2d2b8b8fc38711646044b
akash <aayush45r@gmail.com>
2025-12-20 01:16:39 +0530

    added dry run as well

--- beginner/fileorg.sh
+++ beginner/fileorg.sh
@@ -2,6 +2,13 @@
 
 echo "this is a file organiser"
 
+dry_run=false
+
+if [ "$1" = "--dry-run" ]; then 
+	dry_run=true
+	echo "doing run run!!"
+fi
+
 echo "enter a dir: "
 read dir 
 
@@ -13,12 +20,16 @@ if [ ! -d "$defaultdir" ]; then
 fi
 
 for file in "$defaultdir"/*; do
-	[ -f "${file##*.}" ] || continue
+	[ -f "$file" ] || continue
 
 	extr="${file##*.}"
 	mkdir -p "$defaultdir/$extr"
 
-	mv "$file" "$defaultdir/$extr/"
+	if "$dry_run"; then
+		echo "[dry-run] file $file -> $extr/"
+	else
+		mv "$file" "$defaultdir/$extr/"
+	fi
 
 done
 
